dca345074179c04600d31cf2a563f3a0c90bc19b
RushikeshBBankar <rushikesh1611@gmail.com>
2023-12-27 00:09:52 +0530

    ci report

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   testDir: './tests',
   fullyParallel: true,
   workers: 1,
-  reporter: 'html',
+  reporter: process.env.CI ? 'dot' : 'list',
   use: {
     browserName: 'chromium',
     headless: true,
